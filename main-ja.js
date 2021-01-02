649d71b62d0665507db91b6df54a6ba3e67ecf08
YanaiMaho <67141677+YanaiMaho@users.noreply.github.com>
2021-01-02 18:35:38 +0900

    Update main-ja.js

--- main-ja.js
+++ main-ja.js
@@ -8,7 +8,8 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+
+let storyText = '気温20度で快晴、外出するには最適な日であった。なので:insertx:は:inserty:に行った。そこには:insertz:が偶然居たのでかなり驚いた。 Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
@@ -29,7 +30,7 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob',name);
+    newStory = newStory.replace('梅子',name);
   }
 
   if(document.getElementById("uk").checked) {
